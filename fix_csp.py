ce3cbb331de5aba96ea38ba3df0f76fe53223d99
rosenalon <alon.rosen@idc.ac.il>
2026-04-09 21:21:43 +0200

    Fix blocked YouTube embed on homepage

---
+++ fix_csp.py
@@ -0,0 +1,15 @@
+﻿import os
+
+fpath = 'index.html'
+html = open(fpath, 'rb').read()
+
+# Fix 1: Remove Content-Security-Policy meta tag that blocks iframes
+html = html.replace(b'<meta http-equiv="Content-Security-Policy"', b'<meta http-equiv="X-Disabled-CSP"')
+html = html.replace(b"<meta http-equiv='Content-Security-Policy'", b"<meta http-equiv='X-Disabled-CSP'")
+html = html.replace(b'content-security-policy', b'x-disabled-csp')
+
+# Fix 2: Use youtube-nocookie for better compatibility
+html = html.replace(b'https://www.youtube.com/embed/FiuItIwjx28"', b'https://www.youtube-nocookie.com/embed/FiuItIwjx28" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"')
+
+open(fpath, 'wb').write(html)
+print("Fixed")
